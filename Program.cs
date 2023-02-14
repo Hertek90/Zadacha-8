b77824ed7656ccf06e6b32098b540d3ebe909ce8
«Apolon» <hertek1990@gmail.com>
2023-02-14 22:44:11 +0700

    Zadacha 8

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1)
+{
+    System.Console.Write("Число не подходит");
+}
+else
+{
+    for (int i = 2; i <= number; i = i + 2)
+{
+    System.Console.Write(i+" ");
+}
+}
